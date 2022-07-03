73dafe21a97360152566bb951fcb9f643e43d28d
Patrick Lopes <patricklopes31@yahoo.com.br>
2022-07-03 09:47:59 -0300

    Exercício de Java Script sobre function Onclick em andamento...(Battle Cards DS).

---
+++ M2_A3_Mudanças_de_Estilos_Exercício.js
@@ -0,0 +1,78 @@
+
+function ChangeCard01 () {
+    document.getElementById("aqua_win").style.display = "block";
+    document.getElementById("flash_lose").style.display = "none";
+    document.getElementById("Keep_card_text01").style.display = "block";
+    document.getElementById("aqua_win").style.display = "none";
+    document.getElementById("next_button").style.display = "block";
+}
+
+
+function ChangeCard02 () {
+    document.getElementById("mulher_win").style.display = "block";
+    document.getElementById("cat_lose").style.display = "none";
+    document.getElementById("Keep_card_text02").style.display = "block";
+    document.getElementById("mulher_win").style.display = "none";
+    document.getElementById("next_button").style.display = "block";
+}
+
+function ChangeCard03 () {
+    document.getElementById("arqueiro_win").style.display = "block";
+    document.getElementById("exterminador_lose").style.display = "none";
+    document.getElementById("Keep_card_text03").style.display = "block";
+    document.getElementById("arqueiro_win").style.display = "none";
+    document.getElementById("next_button").style.display = "block";
+}
+
+function ChangeCard04 () {
+    document.getElementById("batman_win").style.display = "block";
+    document.getElementById("drdestino_lose").style.display = "none";
+    document.getElementById("Keep_card_text04").style.display = "block";
+    document.getElementById("batman_win").style.display = "none";
+    document.getElementById("next_button").style.display = "block";
+}
+
+function ChangeCard05 () {
+    document.getElementById("superman_win").style.display = "block";
+    document.getElementById("monstro_lose").style.display = "none";
+    document.getElementById("Keep_card_text05").style.display = "block";
+    document.getElementById("superman_win").style.display = "none";
+    document.getElementById("next_button").style.display = "block";
+}
+
+function ChangeCard06 () {
+    document.getElementById("maravilha_win").style.display = "block";
+    document.getElementById("aquaman_lose").style.display = "none";
+    document.getElementById("Keep_card_text06").style.display = "block";
+    document.getElementById("maravilha_win").style.display = "none";
+    document.getElementById("next_button").style.display = "block";
+}
+
+function ChangeCard07 () {
+    document.getElementById("batman_win").style.display = "block";
+    document.getElementById("arqueiro_lose").style.display = "none";
+    document.getElementById("Keep_card_text07").style.display = "block";
+    document.getElementById("batman_win").style.display = "none";
+    document.getElementById("next_button").style.display = "block";
+}
+
+function ChangeCard08 () {
+    document.getElementById("superman_win").style.display = "block";
+    document.getElementById("maravilha_lose").style.display = "none";
+    document.getElementById("Keep_card_text08").style.display = "block";
+    document.getElementById("superman_win").style.display = "none";
+    document.getElementById("next_button").style.display = "block";
+
+    /*
+    document.getElementById("image_backgroung_change").style.display = "block";
+    */
+}
+
+function ChangeCard09 () {
+    document.getElementById("batman_win").style.display = "block";
+    document.getElementById("superman_lose").style.display = "none";
+    document.getElementById("Keep_card_text09").style.display = "block";
+    document.getElementById("batman_win").style.display = "none";
+    document.getElementById("next_button").style.display = "block";
+    document.getElementById("winner_image").style.display = "block";
+}
